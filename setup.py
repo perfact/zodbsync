8944b4e20eed04c7b4889cb0b64d83276e61ac7a
Simon Pamies <spamsch@gmail.com>
2018-11-28 12:14:07 +0100

    Bumped version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 
 from setuptools import setup
 setup(name='perfact-zodbsync',
-      version='3.10.0',
+      version='3.10.1',
       description='Zope Recorder and Playback',
       long_description=''' ''',
       author='Ján Jockusch et.al.',
