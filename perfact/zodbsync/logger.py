3d943298d3538fcfbfd66179e9ebd50e83be0a5b
Simon Pamies <s.pamies@appweeve.com>
2019-02-27 16:40:46 +0100

    Making code work if no systemd is available

--- perfact/zodbsync/logger.py
+++ perfact/zodbsync/logger.py
@@ -1,4 +1,5 @@
 import logging
+HAS_SYSTEMD = False
 try:
     import systemd.journal
     try:
@@ -6,6 +7,7 @@ try:
     except AttributeError:
         # structure of module changed? check debian-packages?
         logging_handler = systemd.journal.JournaldLogHandler
+    HAS_SYSTEMD = True
 except ImportError:
     # Fall back to a standard syslog handler
     import logging.handlers
@@ -15,6 +17,8 @@ def get_logger(name):
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
     logger.propagate = False
-    logger.addHandler(logging_handler(SYSLOG_IDENTIFIER=name))
+    if HAS_SYSTEMD:
+        logger.addHandler(logging_handler(SYSLOG_IDENTIFIER=name))
+    else: logger.addHandler(logging_handler())
     logger.addHandler(logging.StreamHandler())
     return logger
