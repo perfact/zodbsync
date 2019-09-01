2fa3010aedc4223c3ba66c127cb2c72c31c14004
Viktor Dick <viktor.dick@perfact.de>
2019-09-01 21:41:57 +0200

    Expose methods to read and write objects

--- perfact/zodbsync/__init__.py
+++ perfact/zodbsync/__init__.py
@@ -1,2 +1,3 @@
-from perfact.zodbsync.zodbsync import ZODBSync
+from perfact.zodbsync.zodbsync import ZODBSync, mod_read, mod_write, \
+obj_modtime
 from perfact.zodbsync.watcher import ZODBSyncWatcher as Watcher
