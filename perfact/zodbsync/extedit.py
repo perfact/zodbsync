7dede200a53573f2b80280d60a790f4e601bbc1b
Viktor Dick <vd@perfact.de>
2020-02-13 10:46:27 +0100

    Add source to served file

--- perfact/zodbsync/extedit.py
+++ perfact/zodbsync/extedit.py
@@ -3,6 +3,14 @@
 
 from perfact.zodbsync.zodbsync import mod_read, mod_write
 
+def find_obj(context, path):
+    obj = context
+    for part in path.split('/'):
+        if not part:
+            continue
+        obj = getattr(context, part)
+    return obj
+
 def controlfile(context, path, url):
     '''
     Creates a control file that can be used by an external editor to update the
@@ -16,7 +24,11 @@ def controlfile(context, path, url):
         ('path', path),
         ('auth', context.REQUEST._auth),
     )
-    return ''.join([
+    header = ''.join([
         '{}: {}\n'.format(key, value)
         for key, value in data
     ])
+
+    obj = find_obj(context, path)
+    data = mod_read(obj)
+    return header + data['source']
