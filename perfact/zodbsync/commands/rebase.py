24fdb231190604531bf38fc4967e6f3774346ae5
Viktor Dick <viktor.dick@perfact.de>
2020-04-18 14:46:33 +0200

    Remove stubs for not yet implemented subcommands

--- perfact/zodbsync/commands/rebase.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-from ..subcommand import SubCommand
-
-
-class Rebase(SubCommand):
-    ''' Sub-command to rebase the local master branch onto another branch,
-    playing back any changed files.'''
--- perfact/zodbsync/commands/reset.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-from ..subcommand import SubCommand
-
-
-class Reset(SubCommand):
-    ''' Sub-command to reset the current branch to the state of another branch,
-    playing back any changed files.'''
