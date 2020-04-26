dc52cf9d9fc5689a425e4751e20ede0091fc4718
Viktor Dick <vd@perfact.de>
2020-04-26 12:38:25 +0200

    Auto-stash unstaged changes if picking

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 import setuptools
 setuptools.setup(
     name='perfact-zodbsync',
-    version='3.14.0.dev2',
+    version='3.14.0.dev3',
     description='Zope Recorder and Playback',
     long_description=''' ''',
     author='Ján Jockusch et.al.',
