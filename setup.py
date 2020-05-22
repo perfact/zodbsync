d3845f735cafa98a4d2065641a12f2f49c20fe34
Viktor Dick <vd@perfact.de>
2020-05-22 07:59:10 +0200

    Release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 import setuptools
 setuptools.setup(
     name='perfact-zodbsync',
-    version='3.14.2.dev1',
+    version='3.14.2',
     description='Zope Recorder and Playback',
     long_description=''' ''',
     author='Ján Jockusch et.al.',
