b3d86a26baa8106b6a7335e28d4bf9a044e823a5
Viktor Dick <vd@perfact.de>
2020-06-16 20:16:38 +0200

    Move launcher into perfact.extedit and handle bytes sources

--- perfact/zodbsync/__init__.py
+++ perfact/zodbsync/__init__.py
@@ -1,11 +1,9 @@
 from .zodbsync import mod_read, mod_write, obj_modtime
-from .extedit import controlfile as extedit_controlfile
-from .extedit import update as extedit_update
+from .extedit import launch as extedit_launch
 
 __all__ = [
     'mod_read',
     'mod_write',
     'obj_modtime',
-    'extedit_controlfile',
-    'extedit_update',
+    'extedit_launch',
 ]
--- perfact/zodbsync/extedit.py
+++ perfact/zodbsync/extedit.py
@@ -1,10 +1,54 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from perfact.zodbsync.zodbsync import mod_read, mod_write
+import json
+
+from . import helpers
+from .zodbsync import mod_read, mod_write
+
+
+def launch(context, script, path, source=None, orig_source=None):
+    '''
+    Launcher for external edit.
+
+    If called without a source, it is used to create a control file that
+    contains the authentication header found in the current request and the
+    content of the file that can be found under path.
+
+    If a source is provided, updates the object given by path, but only if the
+    current source matches that given in orig_source.
+
+    A wrapper script should be placed in the top-level of the ZODB that is only
+    accessible for Manager and delegates to this.
+    '''
+
+    resp = context.REQUEST.RESPONSE
+
+    if source is None:
+        content_type = 'application/x-perfact-zopeedit'
+        result = controlfile(
+            context=context,
+            path=path,
+            url=script.absolute_url(),
+        )
+    else:
+        content_type = 'application/json'
+        result = update(
+            context=context,
+            path=path,
+            source=source,
+            orig_source=orig_source,
+        )
+        result = json.dumps(result)
+
+    resp.setHeader('Content-Type', content_type)
+    return result
 
 
 def find_obj(context, path):
+    '''
+    Locate object at given path
+    '''
     obj = context
     for part in path.split('/'):
         if not part:
@@ -17,8 +61,8 @@ def controlfile(context, path, url):
     '''
     Creates a control file that can be used by an external editor to update the
     contents of an object. The control file contains
-    * the entrypoint url (which should be a script in Zope wrapping these
-      methods)
+    * the entrypoint url (which should be a script in Zope wrapping the
+      launcher)
     * an authentication header
     * the path to the object in question
     * the meta_type of the object
@@ -37,6 +81,13 @@ def controlfile(context, path, url):
     obj = find_obj(context, path)
     data = mod_read(obj)
 
+    # This is a hack. It would be better if mod_read always returned string
+    # sources and the different object types transferred them if necessary
+    # (which is only the case in Python 2). But if we change that, we should
+    # also no longer store sources with a -utf8 suffix, which would create a
+    # large diff. To be discussed.
+    data['source'] = helpers.to_string(data['source'])
+
     result += 'type: {type}\n\n{source}'.format(**data)
     return result
 
@@ -49,7 +100,7 @@ def update(context, path, source, orig_source):
     obj = find_obj(context, path)
     data = mod_read(obj)
 
-    if data['source'] != orig_source:
+    if helpers.to_string(data['source']) != orig_source:
         return {'error': 'Object was changed'}
     data['source'] = source
     mod_write(obj, data)
