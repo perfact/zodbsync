f7694cc9c56f583b818c1a5d5d37dd59f991788d
Viktor Dick <viktordick@users.noreply.github.com>
2020-06-29 14:45:41 +0200

    Add launcher for external editor without WebDAV (#31)

--- perfact/zodbsync/__init__.py
+++ perfact/zodbsync/__init__.py
@@ -1,3 +1,9 @@
-from perfact.zodbsync.zodbsync import mod_read, mod_write, obj_modtime
+from .zodbsync import mod_read, mod_write, obj_modtime
+from .extedit import launch as extedit_launch
 
-__all__ = ['mod_read', 'mod_write', 'obj_modtime']
+__all__ = [
+    'mod_read',
+    'mod_write',
+    'obj_modtime',
+    'extedit_launch',
+]
