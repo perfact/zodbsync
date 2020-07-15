perfact/zodbsync

commit 2d00478af901eb66019b0a2d4fa4d6ce9e499c3c

Do not force re-recording of meta data only to append the missing new line, only append it for anyhow changed files

perfact/zodbsync/zodbsync.py
@@ -407,7 +407,7 @@ class ZODBSync:
         except IOError:
             old_data = None
 
-        if old_data != fmt:
+        if old_data.strip() != fmt.strip():
             self.logger.debug("Will write %d bytes of metadata" % len(fmt))
             with open(data_fname, 'wb') as f:
                 f.write(fmt)

commit b962b4f83b05315c199fbf87eaa4412f8fb9a69a

Do not break when recording new objects
This fixes a bug introduced in #35 where new objects could no longer be
recorded because the comparison of old and new metadata was done after
stripping both of whitespaces without considering that one might be
`None`.

perfact/zodbsync/zodbsync.py
@@ -407,7 +407,7 @@ class ZODBSync:
         except IOError:
             old_data = None
 
-        if old_data.strip() != fmt.strip():
+        if old_data is None or old_data.strip() != fmt.strip():
             self.logger.debug("Will write %d bytes of metadata" % len(fmt))
             with open(data_fname, 'wb') as f:
                 f.write(fmt)