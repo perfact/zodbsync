de30a8a870c7a9acb878e75c3264a6f1ca9e5103
Viktor Dick <vd@perfact.de>
2020-12-22 15:46:38 +0100

    Add some watcher tests

--- perfact/zodbsync/tests/environment.py
+++ perfact/zodbsync/tests/environment.py
@@ -88,28 +88,12 @@ class ZODBSyncConfig():
         _, self.path = tempfile.mkstemp()
         with open(self.path, 'w') as f:
             f.write('''
-# Path of the Zope instance configuration to use to
-# instantiate Zope2.app()
-wsgi_conf_path = '{zopeconf}'
-
-# Path to Data.fs which is needed for lookup of object IDs from transaction IDs
-# with perfact-zoperecord --watch
+conf_path = '{zopeconf}'
 datafs_path = '{zeopath}/var/Data.fs'
-
-# user that is used to create commits and as default owner of objects
 manager_user = 'perfact'
-
-# create the manager user on empty databases
 create_manager_user = True
-
-# sets the default owner for objects that have no owner in the file system
-# representation
 default_owner = 'perfact'
-
-# Base directory of the repository
 base_dir = '{repodir}'
-
-# default settings for git repos
 commit_name = "Zope Developer"
 commit_email = "zope-devel@example.de"
 commit_message = "Generic commit message."
