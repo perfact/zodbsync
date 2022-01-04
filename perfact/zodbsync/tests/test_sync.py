4d416cff1c1f8777ec8838987848d6d0dccb59a6
Alexander Rolfes <alexander.rolfes@perfact.de>
2022-01-04 15:03:02 +0100

    Add testcase for changing the prop type

--- perfact/zodbsync/tests/test_sync.py
+++ perfact/zodbsync/tests/test_sync.py
@@ -505,6 +505,29 @@ class TestSync():
         self.run('playback', '/')
         assert self.app.getProperty('testprop') == 'test'
 
+    def test_changeproptype(self):
+        "Change the type of a property"
+        with self.runner.sync.tm:
+            self.app.manage_addProperty(
+                'testprop', 'test', 'string'
+            )
+        fname = self.repo.path + '/__root__/__meta__'
+        self.run('record', '/')
+        with open(fname, 'r') as f:
+            content = f.read()
+        data = dict(helpers.literal_eval(content))
+        prop = {
+            'id': 'testprop',
+            'type': 'int',
+            'value': 1,
+        }
+        data['props'] = [list(prop.items())]
+        with open(fname, 'w') as f:
+            f.write(zodbsync.mod_format(data))
+        self.run('playback', '/')
+        assert self.app.getProperty('testprop') == 1
+        assert self.app.getPropertyType('testprop') == 'int'
+
     def test_cacheable(self):
         "Add a RamCacheManager and use it for index_html"
         self.app.manage_addProduct[
