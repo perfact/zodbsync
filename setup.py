ff4ee998dd974e1815a36688e7dfaaba61cc4151
Viktor Dick <vd@perfact.de>
2022-02-11 16:13:09 +0100

    Release version 4.3.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ else:
 
 setuptools.setup(
     name='perfact-zodbsync',
-    version='4.2.4',
+    version='4.3.0',
     description='Zope Recorder and Playback',
     long_description=''' ''',
     author='Ján Jockusch et.al.',
