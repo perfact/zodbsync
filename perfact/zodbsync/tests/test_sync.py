29129cb31592fc0a7d83b44b2522b8db935fb79d
Alexander Rolfes <alexander.rolfes@perfact.de>
2025-05-12 23:07:09 +0200

    Fix mistake, both tests require the 'blob' file to be added

--- perfact/zodbsync/tests/test_sync.py
+++ perfact/zodbsync/tests/test_sync.py
@@ -2214,6 +2214,9 @@ class TestSync():
         Validate the correct writing and clearing of the layer ident
         in the Data.FS
         """
+        with self.runner.sync.tm:
+            self.app.manage_addProduct['OFSP'].manage_addFile(id='blob')
+
         with self.addlayer(frozen=True) as layer:
             self.run('record', '/blob')
             assert getattr(self.app.blob, 'zodbsync_layer', None) is None
